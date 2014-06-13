059fe749667ecb808372f7783c4880165a757a78
Jody Lognoul <lognoulj@gmail.com>
2014-06-13 12:09:54 +0700

    06-13-2014

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,7 @@ module.exports = function(grunt) {
     // Configurable paths
     var config = {
         app: 'public/app',
-        dist: 'public/dist',
-        tmp: 'public/.tmp'
+        dist: 'public/dist'
     };
 
     // Project configuration.
@@ -18,11 +17,67 @@ module.exports = function(grunt) {
 
         // Project settings
         config: config,
+        
+        // Add vendor prefixed styles
+        autoprefixer: {
+            options: {
+                browsers: ['last 1 version']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.tmp %>/styles/',
+                    src: '{,*/}*.css',
+                    dest: '<%= config.tmp %>/styles/'
+                }]
+            }
+        },
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            js: {
+                files: ['<%= config.app %>/scripts/{,*/}*.js'],
+                tasks: ['jshint']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            compass: {
+                files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
+                tasks: ['compass:dev', 'autoprefixer']
+            },
+            styles: {
+                files: ['<%= config.app %>/styles/{,*/}*.css'],
+                tasks: ['newer:copy:styles', 'autoprefixer']
+            }            
+        },        
+        // Concat the css files
+        concat_css: {
+            all: {
+                src: ["<%= config.tmp %>/styles/*.css"],
+                dest: "<%= config.tmp %>/styles/all.css"
+            },
+        },
+
+        // Minify the css files
+        cssmin: {
+            dist: {
+                files: {
+                    '<%= config.tmp %>/styles/all.min.css': [
+                        '<%= config.tmp %>/styles/all.css'
+                    ]
+                }
+            }
+        },
 
         // Run some tasks in parallel to speed up build process
         concurrent: {
+            dev: [
+                'compass:dev',
+                'copy:styles',
+            ],
             dist: [
-                'sass',
+                'sass:dist',
                 'copy:styles',
                 // 'imagemin',
                 // 'svgmin'
@@ -31,11 +86,18 @@ module.exports = function(grunt) {
 
         // Copies remaining files to places other tasks can use
         copy: {
+            dist: {
+                expand: true,
+                dot: true,
+                cwd: '<%= config.tmp %>/styles',
+                dest: '<%= config.dist %>/styles/',
+                src: '{,*/}*.min.css'
+            },
             styles: {
                 expand: true,
                 dot: true,
                 cwd: '<%= config.app %>/styles',
-                dest: '<%= config.tmp %>/styles/',
+                dest: '<%= config.dist %>/styles/',
                 src: '{,*/}*.css'
             }
         },
@@ -46,12 +108,22 @@ module.exports = function(grunt) {
                 files: [{
                     dot: true,
                     src: [
-                        '<%= config.tmp %>',
-                        '<%= config.dist %>/*'
+                        '<%= config.dist %>'
                     ]
                 }]
             }
         },
+        // Compiles Sass to CSS using Compass
+        compass: {                  
+            dev: {
+                options: {
+                    outputStyle: 'compressed',
+                    importPath: 'public/bower_components/bootstrap-sass-official/vendor/assets/stylesheets',
+                    sassDir: '<%= config.app %>/styles',
+                    cssDir: '<%= config.dist %>/styles'
+                }
+            }
+        },        
         // Compiles Sass to CSS and generates necessary files if requested
         sass: {
             dist: {
@@ -59,21 +131,32 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'public/app/styles',
                     src: ['*.sass'],
-                    dest: '<%= config.tmp %>/styles',
+                    dest: '<%= config.dist %>/styles',
                     ext: '.css'
                 }]
             }
         }
     });
 
+    grunt.registerTask('serve', [
+        'clean:dist',
+        'concurrent:dev',
+        'autoprefixer',
+        'watch'
+    ]);
+
     // Build task(s).
     grunt.registerTask('build', [
         'clean:dist',
-        'concurrent:dist'
+        'concurrent:dev',
+        'autoprefixer',
+        'concat_css:all',
+        'cssmin:dist',
+        'copy:dist'
     ]);
 
 
     // Default task(s).
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['serve']);
 
 };
